7cf9ae003a3b77da0e85c3ed89d3ec1654f1d064
Hatim KHOUZAIMI <hatim.enst@gmail.com>
2015-01-29 22:03:21 +0100

    Modification in index.js so that it prints index.html

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.get('/', function(req, res) {
-    res.send('This is a test!');
+    var fileBuffer = fs.readFileSync("index.html").toString();
+    res.send(fileBuffer);
 })
 
-app.listen(process.env.PORT);
-
-console.log('process.env.PORT: ' + process.env.PORT);
-console.log('app.get(port): ' + app.get('port'));
+app.listen(process.env.PORT || 8080);
