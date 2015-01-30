12517a3f2a83eb4a99a1f0a5e1b7cefb762b6e17
Hatim KHOUZAIMI <hatim.enst@gmail.com>
2015-01-30 19:16:22 +0100

    Adding an audio element

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+app.use(express.static(__dirname));
+
 app.get('/', function(req, res) {
     var fileBuffer = fs.readFileSync("index.html").toString();
     res.send(fileBuffer);
